c5ed789edd32fde8b5f03b57f2e5090cc7a4922d
TeifGuru <TeifGuru@gmail.com>
2024-03-20 19:10:30 +0300

    End program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ void firstArray(string[] stringArray) // Заполнение массива
     for (int i = 0; i < stringArray.Length; i++)
     {
         Console.WriteLine($"Введите {i + 1} элемент массива");
-        stringArray[i] = Console.ReadLine();
+        stringArray[i] = Console.ReadLine()!;
     }
     Console.WriteLine($"Ваш массив: [{string.Join(", ", stringArray)}]");
 }
