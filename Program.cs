bb8f044fbccf44a3da8006573db8eda65961de35
TeifGuru <TeifGuru@gmail.com>
2024-03-20 19:08:26 +0300

    End program

--- Program.cs
+++ Program.cs
@@ -1,27 +1,37 @@
 ﻿Console.Write("Введите количество элементов массива: ");
 int length = Convert.ToInt32(Console.ReadLine());
 string[] stringArray = new string[length];
-for (int i = 0; i < stringArray.Length; i++)
+void firstArray(string[] stringArray) // Заполнение массива
 {
-    Console.WriteLine($"Введите {i + 1} элемент массива");
-    stringArray[i] = Console.ReadLine();
-}
-Console.WriteLine($"Ваш массив: [{string.Join(", ", stringArray)}]");
-int count = 0;
-for (int i = 0; i < stringArray.Length; i++)
-{
-    if (stringArray[i].Length <= 3)
-        count++;
-}
-Console.WriteLine($" Искомых элементов мссива: {count}");
-string[] newArray = new string[count];
-int index = 0;
-for (int i = 0; i < stringArray.Length; i++)
-{
-    if (stringArray[i].Length <= 3)
+    for (int i = 0; i < stringArray.Length; i++)
     {
-        newArray[index] = stringArray[i];
-        index++;
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        stringArray[i] = Console.ReadLine();
     }
+    Console.WriteLine($"Ваш массив: [{string.Join(", ", stringArray)}]");
 }
-Console.WriteLine($"Ваш новый массив: [{string.Join(", ", newArray)}]");
+string[] finalArray(string[] stringArray) // Подсчет колличества искомых элементов массива и вывод нового массива
+{
+    int count = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+            count++;
+    }
+    Console.WriteLine($" Искомых элементов мссива: {count}");
+
+    string[] newArray = new string[count];
+    int index = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            newArray[index] = stringArray[i];
+            index++;
+        }
+    }
+    Console.WriteLine($"Ваш новый массив: [{string.Join(", ", newArray)}]");
+    return newArray;
+}
+firstArray(stringArray);
+finalArray(stringArray);
